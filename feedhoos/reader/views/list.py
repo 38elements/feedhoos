bee2807f190ad405f157efde8bb88d9bc1fece4e
38elements <mh19820223@gmail.com>
2014-02-19 11:28:52 +0900

    list on angular

--- feedhoos/reader/views/list.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-from django.shortcuts import render
-from feedhoos.finder.models.feed import FeedModel
-
-
-def execute(request):
-    feeds = FeedModel.objects.all()
-    return render(request, "reader/list.html", {"feeds": feeds})
