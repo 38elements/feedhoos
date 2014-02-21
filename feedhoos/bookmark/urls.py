64983331eefa3dff88a14f3c910b68bb4ea94ee2
38elements <mh19820223@gmail.com>
2014-02-22 07:56:32 +0900

    add rating function

--- feedhoos/bookmark/urls.py
+++ feedhoos/bookmark/urls.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 from django.conf.urls import patterns, url
 import feedhoos.bookmark.views.list
+import feedhoos.bookmark.views.rating
 
 urlpatterns = patterns(
     '',
     url(r'^list/', feedhoos.bookmark.views.list.execute, name="list"),
+    url(r'^rating/', feedhoos.bookmark.views.rating.execute, name="rating"),
 )
---
+++ feedhoos/bookmark/views/rating.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import json
+from django.http import HttpResponse
+from feedhoos.reader.models.bookmark import BookmarkModel
+
+
+def execute(request):
+    "bookmarkのratingを変更する。"
+    feed_id = request.POST["feed_id"]
+    rating = request.POST["rating"]
+    bookmark_model = BookmarkModel.objects.get(feed_id=feed_id)
+    bookmark_model.rating = int(rating)
+    bookmark_model.save()
+    response_json = json.dumps({"msg": "OK"}, ensure_ascii=False, skipkeys=True)
+    return HttpResponse(response_json, mimetype='application/json')
