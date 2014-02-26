bcb8b8215ef20421e3afa093f7158990d7b39769
38elements <mh19820223@gmail.com>
2014-02-26 09:04:21 +0900

    requestする順番を変更

--- feedhoos/finder/static/js/feedhoos.js
+++ feedhoos/finder/static/js/feedhoos.js
@@ -356,13 +356,13 @@ feedhoosControllers.controller(
 feedhoosControllers.controller("ListCtrl", ["$scope", "$http", "$cookies", "timelineManager", "bookmarkManager",
     "timelineManager", "readingManager",
     function($scope, $http, $cookies, timelineManager, bookmarkManager, timelineManager, readingManager) {
+        bookmarkManager.set($scope, function(scope, that) {
+            scope.bookmark = that.data;
+        });
         timelineManager.set($scope, function(scope, that) {
             //「登録されているすべてのfeedを除外
             var feeds = that.data.filter(function(f) {return f.id != 0});
             scope.feeds = that.sortByRating(feeds);
-        });
-        bookmarkManager.set($scope, function(scope, that) {
-            scope.bookmark = that.data;
         });
         $scope.remove = function(feed_id) {
             bookmarkManager.remove(feed_id);
