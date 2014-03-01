8340d46ef5b263933a42f627ede878d22a85ff72
38elements <mh19820223@gmail.com>
2014-03-01 21:26:54 +0900

    directive分割

---
+++ feedhoos/finder/static/js/directives/rating.js
@@ -0,0 +1,57 @@
+feedhoos.controller("RatingCtrl", ["$scope", "$http", "$cookies", "bookmarkManager", 
+    function($scope, $http, $cookies, bookmarkManager) {
+        $scope.max = 5;
+        $scope.isReadonly = false;
+        $scope.bookmark = null;
+        bookmarkManager.set($scope, function(scope, that) {
+            scope.bookmark = that.data;
+        });
+        $scope.watch_rating = function() {
+            this.unwatch_rating = $scope.$watch("rating", function(new_rating, old_rating) {
+                if (new_rating == old_rating) {
+                    return;
+                }
+                bookmarkManager.set_rating($scope.feed_id, new_rating);
+                var csrftoken = $cookies.csrftoken;
+                $http({
+                     "url": $scope.url,
+                     "method": "POST",
+                     "xsrfHeaderName": "X-CSRFToken",
+                     "xsrfCookieName": "csrftoken",
+                     "headers": {"Content-Type": "application/x-www-form-urlencoded"},
+                     "data": ("id=" + $scope.feed_id + "&rating=" + $scope.rating)
+                }).success(function(data) {
+                    $scope.result = data;
+                });
+            });
+        }
+        while($scope.bookmark === null) {
+        }
+    }]
+);
+
+feedhoos.directive("fhRating", function() {
+    return {
+        replace: true,
+        restrict: "E",
+        scope: {url: "@url"}, 
+        controller: "RatingCtrl",
+        link: function(scope, element, attrs, controller) {
+            scope.rating = scope.bookmark[attrs.feedId + ""].rating;
+            scope.feed_id = attrs.feedId;
+            scope.watch_rating()
+            scope.$watch(function() { 
+                    return element.attr("feed-id");
+            },
+            function (new_feed_id, old_feed_id) {
+                if (new_feed_id != old_feed_id) {
+                    scope.unwatch_rating();
+                    scope.rating = scope.bookmark[attrs.feedId + ""].rating;
+                    scope.feed_id = attrs.feedId;
+                    scope.watch_rating();
+                }
+            });
+        },
+        template: '<span><span class="glyphicon glyphicon-arrow-left" ng-click="rating = 0"></span> <rating value="rating" max="max" readonly="false"></rating></span>'
+    }
+});
---
+++ feedhoos/finder/static/js/directives/remove_feed.js
@@ -0,0 +1,36 @@
+feedhoos.controller("RemoveFeedCtrl", ["$scope", "$http", "$cookies", "bookmarkManager",
+    "timelineManager", "readingManager",
+    function($scope, $http, $cookies, timelineManager, bookmarkManager, readingManager) {
+        $scope.remove = function() {
+            bookmarkManager.remove($scope.feed_id);
+            timelineManager.remove($scope.feed_id);
+            readingManager.remove($scope.feed_id);
+            var csrftoken = $cookies.csrftoken;
+            $http({
+                 "url": "/reader/feed/list/delete/",
+                 "method": "POST",
+                 "xsrfHeaderName": "X-CSRFToken",
+                 "xsrfCookieName": "csrftoken",
+                 "headers": {'Content-Type': 'application/x-www-form-urlencoded'},
+                 "data": "id=" + $scope.feed_id
+            }).success(function(data) {
+                //FIXME
+            });
+        }
+    }]
+);
+
+feedhoos.directive("fhRemoveFeed", function() {
+    return {
+        replace: true,
+        restrict: "E",
+        scope: {
+            feed_id: "@feedId",
+            url: "@url",
+        }, 
+        controller: "RemoveFeedCtrl",
+        link: function(scope, element, attrs, controller) {
+        },
+        template: '<span class="glyphicon glyphicon-remove" ng-click="remove()"></span>'
+    }
+});
