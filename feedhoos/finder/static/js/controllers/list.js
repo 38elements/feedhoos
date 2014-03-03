392fe206b901b89bb3227311ff8dc3c5e554dc18
38elements <mh19820223@gmail.com>
2014-03-03 15:08:44 +0900

    リストページの左側登録しているすべてのfeedの項目を追加

--- feedhoos/finder/static/js/controllers/list.js
+++ feedhoos/finder/static/js/controllers/list.js
@@ -2,6 +2,10 @@ feedhoosControllers.controller("ListCtrl", ["$scope", "$http", "$cookies",  "boo
     "timelineManager",
     function($scope, $http, $cookies, bookmarkManager, timelineManager) {
         $scope.is_add_folder = true;
+        $scope.active_folder_id = 0;
+        $scope.select_folder = function(folder_id) {
+            console.log(folder_id);
+        };
         $scope.create_folder = function() {
             console.log($scope.folder_name);
         }
