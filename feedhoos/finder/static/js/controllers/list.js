59aefc92233c61b3ff50ff4f6cd0d1de345aac35
38elements <mh19820223@gmail.com>
2014-03-06 12:18:24 +0900

    sortByRatingの前にwait

--- feedhoos/finder/static/js/controllers/list.js
+++ feedhoos/finder/static/js/controllers/list.js
@@ -1,6 +1,6 @@
-feedhoosControllers.controller("ListCtrl", ["$scope", "$http", "$cookies",  "bookmarkManager",
+feedhoosControllers.controller("ListCtrl", ["$scope", "$http", "$cookies", "$timeout",  "bookmarkManager",
     "timelineManager", "folderManager",
-    function($scope, $http, $cookies, bookmarkManager, timelineManager, folderManager) {
+    function($scope, $http, $cookies, $timeout, bookmarkManager, timelineManager, folderManager) {
         $scope.is_add_folder = true;
         $scope.active_folder_id = 0;
         $scope.select_folder = function(folder_id) {
@@ -16,9 +16,14 @@ feedhoosControllers.controller("ListCtrl", ["$scope", "$http", "$cookies",  "boo
             scope.bookmark = that.data;
         });
         timelineManager.set($scope, function(scope, that) {
+            var predict_func = function() {
+                return scope.bookmark === null;
+            }
             //「登録されているすべてのfeedを除外
             var feeds = that.data.filter(function(f) {return f.id != 0});
-            scope.feeds = that.sortByRating(feeds);
+            that.wait($timeout, 25, predict_func,  function() {
+                scope.feeds = that.sortByRating(feeds);
+            });
         });
     }]
 );
