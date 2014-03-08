8ce4dc1eef182877b9e956c03c0916e9fdcff3ad
38elements <mh19820223@gmail.com>
2014-03-08 16:33:42 +0900

    add folder_id on bookmark.

--- feedhoos/reader/models/bookmark.py
+++ feedhoos/reader/models/bookmark.py
@@ -7,14 +7,20 @@ class BookmarkModel(models.Model):
     feed_id = models.IntegerField(unique=True)
     last_updated = models.IntegerField(default=0)
     rating = models.PositiveSmallIntegerField(default=0)
+    folder_id = models.IntegerField(default=0, db_index=True)
 
     @staticmethod
     def get_json(bookmark_models):
         bookmarks_dict = {
-            "0": {"rating": 6, "type": "feed"}
+            "0": {
+                "rating": 6,
+                "type": "feed",
+                "folder_id": 0
+            }
         }
         for b in bookmark_models:
             bookmarks_dict[str(b.feed_id)] = {
+                "folder_id": b.folder_id,
                 "rating": b.rating,
                 "type": "feed"
             }
