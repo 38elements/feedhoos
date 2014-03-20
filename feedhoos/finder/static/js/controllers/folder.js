be24ffe7a6a79eebc34891bd13a6a42d052c41a0
38elements <mh19820223@gmail.com>
2014-03-20 21:18:19 +0900

    add folder template

--- feedhoos/finder/static/js/controllers/folder.js
+++ feedhoos/finder/static/js/controllers/folder.js
@@ -1,5 +1,8 @@
 feedhoosControllers.controller("FolderCtrl", ["$scope", "$timeout", "folderManager",
     function($scope, $timeout, folderManager) {
+        $scope.is_ready = function() {
+            return $scope.folders !== undefined;
+        }
         folderManager.set($scope, function(scope, that) {
             scope.folders = that.data;
         }); 
