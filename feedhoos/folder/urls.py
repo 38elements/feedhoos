a0d19c55f47f9b74eda65b4d6cd1c1c776d6a811
38elements <mh19820223@gmail.com>
2014-03-21 21:03:55 +0900

    folderにrating機能を追加

--- feedhoos/folder/urls.py
+++ feedhoos/folder/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 import feedhoos.folder.views.create
 import feedhoos.folder.views.list
 import feedhoos.folder.views.delete
+import feedhoos.folder.views.rating
 
 
 urlpatterns = patterns(
@@ -10,4 +11,5 @@ urlpatterns = patterns(
     url(r'^create/$', feedhoos.folder.views.create.execute, name="create"),
     url(r'^list/$', feedhoos.folder.views.list.execute, name="list"),
     url(r'^delete/$', feedhoos.folder.views.delete.execute, name="delete"),
+    url(r'^rating/$', feedhoos.folder.views.rating.execute, name="rating"),
 )
---
+++ feedhoos/folder/views/rating.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import json
+from django.http import HttpResponse
+from feedhoos.folder.models.folder import FolderModel
+
+
+def execute(request):
+    "folderのratingを変更する。"
+    folder_id = request.POST["id"]
+    rating = request.POST["rating"]
+    folder_model = FolderModel.objects.get(pk=folder_id)
+    folder_model.rating = int(rating)
+    folder_model.save()
+    response_json = json.dumps({"msg": "OK"}, ensure_ascii=False, skipkeys=True)
+    return HttpResponse(response_json, mimetype='application/json')
