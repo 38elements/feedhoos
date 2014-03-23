6d27314ab82270e73b69e7ab4e276c1d683525ee
38elements <mh19820223@gmail.com>
2014-03-23 23:09:20 +0900

    short cut keyが一番下までスクロールすると機能しない

--- feedhoos/finder/static/js/controllers/reader.js
+++ feedhoos/finder/static/js/controllers/reader.js
@@ -52,20 +52,33 @@ feedhoosControllers.controller(
         }
     }]
 )
-.directive("scrollTop", function() {
+.directive("scrollTop", function($window, $timeout) {
     return {
         restrict: "A",
         link: function(scope, element, attrs, ReaderCtrl) {
             scope.$watch(function() {
                 if (scope.feed) {
-                    return (scope.feed.id + scope.type);
+                    return (scope.feed.id + scope.type + scope.feed.type);
                 }
                 else {
                     return scope.feed;
                 }
             }, function() {
                 element[0].scrollIntoView();
+                $timeout(
+                    function() {
+                        var content_bar = $window.document.getElementById("content_bar");
+                        var content_bar_top = content_bar.getBoundingClientRect().top 
+                        var innerHeight = $window.innerHeight;
+                        var last_entry = content_bar.querySelector(".entry.panel-default:nth-last-of-type(1)");
+                        if (last_entry 
+                                && parseInt($window.getComputedStyle(last_entry, "").height) < (innerHeight - content_bar_top)) {
+                            last_entry.style.height = innerHeight - content_bar_top + "px";
+                        }
+                    },
+                    1
+                );
             });
         },
     }
-});
+})
