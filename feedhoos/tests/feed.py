21fbe2a4cfc67de1b1b841835254648dceb21488
38elements <mh19820223@gmail.com>
2014-04-05 23:16:13 +0900

    add FeedModelTestCase::test_new_entries

--- feedhoos/tests/feed.py
+++ feedhoos/tests/feed.py
@@ -38,12 +38,18 @@ class FeedModelTestCase(TestCase):
 
     def test_last_updated(self):
         feed_model1 = FeedModel.objects.get(pk=3)
-        l = 1500000000
+        l = 1396704170
         self.assertEqual(feed_model1.last_updated, l)
         feed_model2 = FeedModel.objects.get(pk=4)
         self.assertEqual(feed_model2.last_updated, 0)
 
+    def test_new_entries(self):
+        feed_model = FeedModel.objects.get(pk=3)
+        new_entries = feed_model.new_entries
+        self.assertEqual(len(new_entries), 1)
+        self.assertEqual(new_entries[0]["updated"], '2014-04-05T13:22:51Z')
+
     def test_feed(self):
         feed_model = FeedModel.objects.get(pk=3)
         feed_model.feed
-        self.assertEqual(len(feed_model.entries), 2)
+        self.assertEqual(len(feed_model.entries), 3)
