9df9656c972b00fc7f12acd4a73e8967f1a713a8
38elements <mh19820223@gmail.com>
2014-04-19 20:24:06 +0900

    add test readingManager::sortByRating()

--- feedhoos/finder/static/js/test/services/reading_manager_spec.js
+++ feedhoos/finder/static/js/test/services/reading_manager_spec.js
@@ -73,10 +73,10 @@ describe("readingManager", function() {
                 '"0": {"rating": 6, "folder_id": 0},' +
                 '"1": {"rating": 3, "folder_id": 1},' +
                 '"2": {"rating": 0, "folder_id": 0},' +
-                '"3": {"rating": 1, "folder_id": 2},' +
+                '"3": {"rating": 0, "folder_id": 2},' +
                 '"4": {"rating": 3, "folder_id": 6},' +
                 '"5": {"rating": 0, "folder_id": 0},' +
-                '"6": {"rating": 0, "folder_id": 0},' +
+                '"6": {"rating": 5, "folder_id": 0},' +
                 '"7": {"rating": 1, "folder_id": 2}' +
             '}');
         readingManager.set(scope, function() {});
@@ -96,7 +96,7 @@ describe("readingManager", function() {
             }, 
             { 
                 "id": 3, 
-                "rating": 1,
+                "rating": 0,
                 "link": "http://example.com/entry/", 
                 "title": "テクノロジー", 
                 "type": "feed", 
@@ -105,13 +105,58 @@ describe("readingManager", function() {
             }, 
             { 
                 "id": 6, 
-                "rating": 0,
+                "rating": 5,
                 "link": "http://example.com/entry/", 
                 "title": "テクノ", 
                 "type": "feed", 
                 "unread_count": 106, 
                 "url": "http://example.com/entry.rss"
             } 
+        ];
+        expect(expecting_data).toEqual(data);
+    }));
+
+    it('should sortByRating sort data by rating of bookmarkManager by join id ',inject(function(readingManager, bookmarkManager) {
+        $httpBackend.expect("GET", "/bookmark/list/")
+            .respond('{' +
+                '"0": {"rating": 6, "folder_id": 0},' +
+                '"1": {"rating": 3, "folder_id": 1},' +
+                '"2": {"rating": 0, "folder_id": 0},' +
+                '"3": {"rating": 0, "folder_id": 2},' +
+                '"4": {"rating": 3, "folder_id": 6},' +
+                '"5": {"rating": 0, "folder_id": 0},' +
+                '"6": {"rating": 5, "folder_id": 0},' +
+                '"7": {"rating": 1, "folder_id": 2}' +
+            '}');
+        readingManager.set(scope, function() {});
+        bookmarkManager.set(scope, function() {});
+        $httpBackend.flush();
+        var data = readingManager.sortByRating(readingManager.data);
+        expecting_data = [ 
+            { 
+                "id": 6, 
+                "link": "http://example.com/entry/", 
+                "title": "テクノ", 
+                "type": "feed", 
+                "unread_count": 106, 
+                "url": "http://example.com/entry.rss"
+            }, 
+            { 
+                "id": 1, 
+                "link": "http://example.com/hotentry/it", 
+                "title": "人気エントリー - テクノロジー", 
+                "type": "feed", 
+                "unread_count": 41, 
+                "url": "http://example.com/hotentry/it.rss"
+            }, 
+            { 
+                "id": 3, 
+                "link": "http://example.com/entry/", 
+                "title": "テクノロジー", 
+                "type": "feed", 
+                "unread_count": 5, 
+                "url": "http://example.com/entry.rss"
+            }, 
         ];
         expect(expecting_data).toEqual(data);
     }));
