29ccce7d1e8d8c452a9dd7c023e14d426333d8d0
38elements <mh19820223@gmail.com>
2014-04-19 20:07:03 +0900

    add test for readManager::attachRating

--- feedhoos/finder/static/js/test/services/reading_manager_spec.js
+++ feedhoos/finder/static/js/test/services/reading_manager_spec.js
@@ -66,4 +66,53 @@ describe("readingManager", function() {
         $httpBackend.flush();
         expect(readingManager.data).toEqual(response_data);
     }));
+
+    it('should attachRating is add rating property timeline feed data',inject(function(readingManager, bookmarkManager) {
+        $httpBackend.expect("GET", "/bookmark/list/")
+            .respond('{' +
+                '"0": {"rating": 6, "folder_id": 0},' +
+                '"1": {"rating": 3, "folder_id": 1},' +
+                '"2": {"rating": 0, "folder_id": 0},' +
+                '"3": {"rating": 1, "folder_id": 2},' +
+                '"4": {"rating": 3, "folder_id": 6},' +
+                '"5": {"rating": 0, "folder_id": 0},' +
+                '"6": {"rating": 0, "folder_id": 0},' +
+                '"7": {"rating": 1, "folder_id": 2}' +
+            '}');
+        readingManager.set(scope, function() {});
+        bookmarkManager.set(scope, function() {});
+        $httpBackend.flush();
+        readingManager.attachRating();
+        var data =  readingManager.data;
+        var expecting_data = [ 
+            { 
+                "id": 1, 
+                "rating": 3,
+                "link": "http://example.com/hotentry/it", 
+                "title": "人気エントリー - テクノロジー", 
+                "type": "feed", 
+                "unread_count": 41, 
+                "url": "http://example.com/hotentry/it.rss"
+            }, 
+            { 
+                "id": 3, 
+                "rating": 1,
+                "link": "http://example.com/entry/", 
+                "title": "テクノロジー", 
+                "type": "feed", 
+                "unread_count": 5, 
+                "url": "http://example.com/entry.rss"
+            }, 
+            { 
+                "id": 6, 
+                "rating": 0,
+                "link": "http://example.com/entry/", 
+                "title": "テクノ", 
+                "type": "feed", 
+                "unread_count": 106, 
+                "url": "http://example.com/entry.rss"
+            } 
+        ];
+        expect(expecting_data).toEqual(data);
+    }));
 });
