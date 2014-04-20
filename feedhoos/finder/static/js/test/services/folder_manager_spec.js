7994360ca8819eb8556e8676dec46ce769b8c0b5
38elements <mh19820223@gmail.com>
2014-04-20 22:44:27 +0900

    add test for folderManager::get_title()

--- feedhoos/finder/static/js/test/services/folder_manager_spec.js
+++ feedhoos/finder/static/js/test/services/folder_manager_spec.js
@@ -7,16 +7,29 @@ describe("folderManager", function() {
         scope = $rootScope.$new();
         $httpBackend = _$httpBackend_;
         $httpBackend.expect("GET", "/folder/list/")
-            .respond('[{"rating": 1, "type": "folder", "id": 1, "title": "title1"}, {"rating": 5, "type": "folder", "id": 2, "title": "title2"}]');
+            .respond('[{"rating": 1, "type": "folder", "id": 1, "title": "title1"}, {"rating": 5, "type": "folder", "id": 2, "title": "title2"}, {"rating": 0, "type": "folder", "id": 3, "title": "title3"}]');
         response_data = [
             {"rating": 1, "type": "folder", "id": 1, "title": "title1"},
-            {"rating": 5, "type": "folder", "id": 2, "title": "title2"}
+            {"rating": 5, "type": "folder", "id": 2, "title": "title2"},
+            {"rating": 0, "type": "folder", "id": 3, "title": "title3"}
         ];
     }));
 
     it('should set', inject(function(folderManager) {
         folderManager.set(scope, function() {});
         $httpBackend.flush();
-        expect(folderManager.data.length).toEqual(2);
+        expect(folderManager.data).toEqual(response_data);
+    }));
+
+    it('should defalt title is ---', inject(function(folderManager) {
+        folderManager.set(scope, function() {});
+        $httpBackend.flush();
+        expect(folderManager.get_title(0)).toEqual('---');
+    }));
+
+    it('should title', inject(function(folderManager) {
+        folderManager.set(scope, function() {});
+        $httpBackend.flush();
+        expect(folderManager.get_title(2)).toEqual('title2');
     }));
 });
