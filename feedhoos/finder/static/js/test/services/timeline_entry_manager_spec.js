c2d2ee61e53350677a0e7a3a15c74561250e9052
38elements <mh19820223@gmail.com>
2014-05-04 08:41:30 +0900

    add test

--- feedhoos/finder/static/js/test/services/timeline_entry_manager_spec.js
+++ feedhoos/finder/static/js/test/services/timeline_entry_manager_spec.js
@@ -17,7 +17,6 @@ describe("timelineEntryManager", function() {
         scope._feed_id = 1;
         scope.type = timelineEntryManager.type;
         scope.active_timeline_type = "feed";
-        timelineEntryManager._is_skip(scope, 1, "feed");
         expect(timelineEntryManager._is_skip(scope, 1, "feed")).toEqual(true);
         expect(timelineEntryManager._is_skip(scope, 2, "feed")).toEqual(false);
         expect(timelineEntryManager._is_skip(scope, 1, "folder")).toEqual(false);
@@ -39,5 +38,52 @@ describe("timelineEntryManager", function() {
         expect(timelineEntryManager.store["2"]).toEqual({feed:{id:2}});
         expect(timelineEntryManager.store["1"]).toEqual({feed:{id:1}});
         
+    }));
+
+    it('should read_feed _is_skip', inject(function(timelineEntryManager) {
+        scope._feed_id = 1;
+        scope.type = timelineEntryManager.type;
+        scope.active_timeline_type = "feed";
+        expect(timelineEntryManager.read_feed(scope, 1, "feed")).toEqual(undefined);
+    }));
+
+    it('should read_feed in store', inject(function(timelineEntryManager) {
+        $httpBackend.expect("GET", "/reader/feed/timeline/1/page/1/")
+            .respond('{' +
+                '"feed": {"id": 1},' +
+                '"entries": []' +
+            '}');
+        timelineEntryManager.set_entries(scope, 1);
+        $httpBackend.flush();
+        scope._feed_id = 2;
+        scope.type = timelineEntryManager.type;
+        scope.active_timeline_type = "feed";
+        timelineEntryManager.read_feed(scope, 1, "feed"); 
+        expect(scope.entries).toEqual([]);
+        expect(scope.feed).toEqual({id: 1});
+    }));
+
+    it('should read_feed set_entries', inject(function(timelineEntryManager) {
+        $httpBackend.expect("GET", "/reader/feed/timeline/1/page/1/")
+            .respond('{' +
+                '"feed": {"id": 1},' +
+                '"entries": []' +
+            '}');
+        scope._feed_id = 2;
+        scope.type = timelineEntryManager.type;
+        scope.active_timeline_type = "feed";
+        timelineEntryManager.read_feed(scope, 1, "feed"); 
+        $httpBackend.flush();
+        expect(scope.entries).toEqual([]);
+        expect(scope.feed).toEqual({id: 1});
+    }));
+
+    it('should remove', inject(function(timelineEntryManager) {
+        timelineEntryManager.store = {
+            "1": 1,
+            "2": 2
+        };
+        timelineEntryManager.remove(1); 
+        expect(timelineEntryManager.store).toEqual({"2": 2});
     }));
 });
