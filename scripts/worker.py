4454162ba830a4d09f12962dda6a3d203a0c742d
38elements <mh19820223@gmail.com>
2014-05-18 10:59:47 +0900

    fix path

--- scripts/worker.py
+++ scripts/worker.py
@@ -6,14 +6,14 @@ sys.path.append("/path/to/feedhoos")
 os.environ['DJANGO_SETTINGS_MODULE'] = "feedhoos.settings"
 from feedhoos.apps.finder.models.feed import FeedModel
 
-with open('/home/miya/worker.txt', "a") as f:
+with open('/path/to/feedhoos/worker.txt', "a") as f:
     ds = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n"
     f.write("start: " + ds)
     f.close()
 
 map(lambda f: f.add_entries(), FeedModel.objects.all())
 
-with open('/home/miya/worker.txt', "a") as f:
+with open('/path/to/feedhoos/worker.txt', "a") as f:
     ds = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n"
     f.write("end: " + ds)
     f.close()
