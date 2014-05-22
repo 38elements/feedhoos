a00165a97def96ab77cd24a10a4ec35a352138d2
38elements <mh19820223@gmail.com>
2014-05-22 23:10:19 +0900

    watchに二重にハンドラを登録しないようにする。

--- feedhoos/finder/static/js/services/base_manager.js
+++ feedhoos/finder/static/js/services/base_manager.js
@@ -1,13 +1,27 @@
 (function () {
     function baseManager($rootScope) {
         this.set = function(scope, callback, message, is_reset) {
-            var that = this;
-            message = message || this.message; 
-            scope.$on(message, function() {
-                callback(scope, that);
-            });
+            var that = this,
+                current_scope,
+                message = message || this.message; 
             if (is_reset) {
                 this.data = null;
+                scope.$$listeners[message] = [];
+                current_scope = scope;
+                do {
+                    if (message in current_scope.$$listenerCount && current_scope.$$listenerCount[message] >= 1) {
+                        current_scope.$$listenerCount[message] -= 1;
+                        if (current_scope.$$listenerCount[message] === 0) {
+                            delete current_scope.$$listenerCount[message];
+                        }
+                    }
+                } while ((current_scope = current_scope.$parent));
+            }
+            //1つのメッセージあたり1つのハンドラしか登録できないようにする。
+            if (!Array.isArray(scope.$$listeners[message]) || scope.$$listeners[message].length < 1) {
+                scope.$on(message, function() {
+                    callback(scope, that);
+                });
             }
             if (this.data === null) {
                 this.$http.get(this.url).success(function(data) {
