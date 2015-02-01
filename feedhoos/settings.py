d87de57b191b35b2703c7dcf7a2c5864d59c0047
38elements <mh19820223@gmail.com>
2015-02-01 14:18:21 +0900

    change setting

--- feedhoos/settings.py
+++ feedhoos/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+#    'debug_toolbar',
     'feedhoos.finder',
     'feedhoos.reader',
     'feedhoos.worker',
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'feedhoos.urls'
@@ -93,19 +93,19 @@ STATICFILES_DIR = ([os.path.join(BASE_DIR, 'feedhoos/finder/static')])
 TEMPLATE_DIRS = os.path.join(BASE_DIR, 'feedhoos/templates')
 
 #django-debug-toolbarの設定
-INTERNAL_IPS = ('127.0.0.1')
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-)
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
+#INTERNAL_IPS = ('127.0.0.1')
+#DEBUG_TOOLBAR_PANELS = (
+#    'debug_toolbar.panels.versions.VersionsPanel',
+#    'debug_toolbar.panels.timer.TimerPanel',
+#    'debug_toolbar.panels.settings.SettingsPanel',
+#    'debug_toolbar.panels.headers.HeadersPanel',
+#    'debug_toolbar.panels.request.RequestPanel',
+#    'debug_toolbar.panels.sql.SQLPanel',
+#    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+#    'debug_toolbar.panels.templates.TemplatesPanel',
+#    'debug_toolbar.panels.cache.CachePanel',
+#    'debug_toolbar.panels.signals.SignalsPanel',
+#    'debug_toolbar.panels.logging.LoggingPanel',
+#    'debug_toolbar.panels.redirects.RedirectsPanel',
+#)
+#DEBUG_TOOLBAR_PATCH_SETTINGS = False
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    exit 1
-fi
+# if [ $# -eq 0 ]; then
+#     exit 1
+# fi
 
-python manage.py runserver $1
+python manage.py runserver 127.0.0.1:8000
